b0529ca742e4917b0e84c2ada98ec1fd81244462
Xiao Fan <fweiwei.026@gmail.com>
2016-08-28 19:41:19 +0800

    Super class for all WeChat function modules.

--- wechatkit/src/main/java/me/ixfan/wechatkit/WeChatFunctionModule.java
+++ wechatkit/src/main/java/me/ixfan/wechatkit/WeChatFunctionModule.java
@@ -21,10 +21,87 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package me.ixfan.wechatkit;
+
+import me.ixfan.wechatkit.token.TokenManager;
 
 /**
  * @author Warren Fan
  */
-public class WeChatFunctionModule {
+public abstract class WeChatFunctionModule {
+
+    protected String appId;
+    protected String appSecret;
+    protected String wechatAccountId;
+    protected TokenManager tokenManager;
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+
+    public String getWechatAccountId() {
+        return wechatAccountId;
+    }
+
+    public void setWechatAccountId(String wechatAccountId) {
+        this.wechatAccountId = wechatAccountId;
+    }
+
+    public TokenManager getTokenManager() {
+        return tokenManager;
+    }
+
+    public void setTokenManager(TokenManager tokenManager) {
+        this.tokenManager = tokenManager;
+    }
+
+    public WeChatFunctionModule(TokenManager tokenManager) {
+        this.tokenManager = tokenManager;
+    }
+
+    public WeChatFunctionModule(String appId, String appSecret) {
+        this.appId = appId;
+        this.appSecret = appSecret;
+    }
+
+    public WeChatFunctionModule(String wechatAccountId, TokenManager tokenManager) {
+        this.wechatAccountId = wechatAccountId;
+        this.tokenManager = tokenManager;
+    }
 }
