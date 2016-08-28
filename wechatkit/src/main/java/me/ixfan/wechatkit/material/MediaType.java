cd15b04273a3b397ccca81fd42eb6e0cc542f75b
Xiao Fan <fweiwei.026@gmail.com>
2016-08-28 17:30:26 +0800

    Available media file types that WeChat supports.

--- wechatkit/src/main/java/me/ixfan/wechatkit/material/MediaType.java
+++ wechatkit/src/main/java/me/ixfan/wechatkit/material/MediaType.java
@@ -21,10 +21,44 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package me.ixfan.wechatkit.material;
 
 /**
+ * 微信素材的媒体文件类型。
+ *
  * @author Warren Fan
  */
-public class MediaType {
+public enum MediaType {
+
+    IMAGE { @Override public String stringValue() { return "image"; } },
+    VOICE { @Override public String stringValue() { return "voice"; } },
+    VIDEO { @Override public String stringValue() { return "video"; } },
+    THUMB { @Override public String stringValue() { return "thumb"; } },
+    NEWS { @Override public String stringValue() { return "news"; } };
+
+    public abstract String stringValue();
 }
