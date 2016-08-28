0dbdc689ae77342e9237944fd13608f2012d42c2
Xiao Fan <fweiwei.026@gmail.com>
2016-08-28 21:23:49 +0800

    Article domain model for uploading.

---
+++ wechatkit/src/main/java/me/ixfan/wechatkit/material/model/ArticleForUpload.java
@@ -0,0 +1,154 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package me.ixfan.wechatkit.material.model;
+
+/**
+ * @author Warren Fan
+ */
+public class ArticleForUpload {
+
+    /**
+     * 标题
+     */
+    private String title;
+    /**
+     * 作者
+     */
+    private String author;
+    /**
+     * 图文消息的摘要，仅有单图文消息才有摘要，多图文此处为空。
+     */
+    private String digest;
+    /**
+     * 图文消息的封面图片素材id（必须是永久mediaID）。
+     */
+    private String thumb_media_id;
+    /**
+     * 是否显示封面，0为false，即不显示，1为true，即显示。
+     */
+    private int show_cover_pic;
+    /**
+     * 图文消息的具体内容，支持HTML标签，必须少于2万字符，小于1M，且此处会去除JS。
+     */
+    private String content;
+    /**
+     * 图文消息的原文地址，即点击“阅读原文”后的 URL。
+     */
+    private String contentSourceUrl;
+
+    /**
+     * 图文消息。
+     *
+     * @param title 标题
+     * @param author 作者
+     * @param contentSourceUrl 图文消息的原文地址，即点击“阅读原文”后的 URL
+     * @param isShowCoverPicture 是否显示封面
+     */
+    public ArticleForUpload(String title, String author, String contentSourceUrl, boolean isShowCoverPicture) {
+        this.title = title;
+        this.author = author;
+        this.contentSourceUrl = contentSourceUrl;
+        this.show_cover_pic = isShowCoverPicture ? 1:0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDigest() {
+        return digest;
+    }
+
+    public void setDigest(String digest) {
+        this.digest = digest;
+    }
+
+    public String getThumb_media_id() {
+        return thumb_media_id;
+    }
+
+    public void setThumb_media_id(String thumb_media_id) {
+        this.thumb_media_id = thumb_media_id;
+    }
+
+    public int getShow_cover_pic() {
+        return show_cover_pic;
+    }
+
+    public void setShow_cover_pic(int show_cover_pic) {
+        this.show_cover_pic = show_cover_pic;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContentSourceUrl() {
+        return contentSourceUrl;
+    }
+
+    public void setContentSourceUrl(String contentSourceUrl) {
+        this.contentSourceUrl = contentSourceUrl;
+    }
+}
