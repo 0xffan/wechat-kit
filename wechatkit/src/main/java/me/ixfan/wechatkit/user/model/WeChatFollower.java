f0d7ef19887391186f0ba88b66d75d52892fda4b
Xiao Fan <fweiwei.026@gmail.com>
2016-09-06 23:43:41 +0800

    Add javadoc.

---
+++ wechatkit/src/main/java/me/ixfan/wechatkit/user/model/WeChatFollower.java
@@ -0,0 +1,232 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package me.ixfan.wechatkit.user.model;
+
+/**
+ * Wechat user model.
+ *
+ * @author xfan
+ */
+public class WeChatFollower {
+
+    /**
+     * 用户的标识，对当前公众号唯一。
+     */
+    private String openid;
+    /**
+     * 用户的昵称。
+     */
+    private String nickname;
+    /**
+     * 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知。
+     */
+    private int sex;
+    /**
+     * 用户所在国家。
+     */
+    private String country;
+    /**
+     * 用户所在省份。
+     */
+    private String province;
+    /**
+     * 用户所在城市。
+     */
+    private String city;
+    /**
+     * 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+     */
+    private String headimgurl;
+    /**
+     * 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
+     */
+    private String unionid;
+    /**
+     * 公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注。
+     */
+    private String remark;
+    /**
+     * 用户所在的分组ID（兼容旧的用户分组接口）。
+     */
+    private int groupid;
+    /**
+     * 用户被打上的标签ID列表。
+     */
+    private int[] tagidList;
+    /**
+     * 用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
+     */
+    private int subscribe;
+    /**
+     * 用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间。
+     */
+    private Long subscribeTime;
+
+    public WeChatFollower(String openid) {
+        this.openid = openid;
+    }
+
+    /**
+     * @return 用户的标识(openid)，对当前公众号唯一。
+     */
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    /**
+     * @return 用户的昵称。
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    /**
+     * @return 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知。
+     */
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * @return 用户所在国家。
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return 用户所在省份。
+     */
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    /**
+     * @return 用户所在城市。
+     */
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+     */
+    public String getHeadimgurl() {
+        return headimgurl;
+    }
+
+    public void setHeadimgurl(String headimgurl) {
+        this.headimgurl = headimgurl;
+    }
+
+    /**
+     * @return 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
+     */
+    public String getUnionid() {
+        return unionid;
+    }
+
+    public void setUnionid(String unionid) {
+        this.unionid = unionid;
+    }
+
+    /**
+     * @return 公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注。
+     */
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    /**
+     * @return 用户所在的分组ID（兼容旧的用户分组接口）。
+     */
+    public int getGroupid() {
+        return groupid;
+    }
+
+    public void setGroupid(int groupid) {
+        this.groupid = groupid;
+    }
+
+    /**
+     * @return 用户被打上的标签ID列表。
+     */
+    public int[] getTagidList() {
+        return tagidList;
+    }
+
+    public void setTagidList(int[] tagidList) {
+        this.tagidList = tagidList;
+    }
+
+    /**
+     * @return 用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
+     */
+    public int getSubscribe() {
+        return subscribe;
+    }
+
+    public void setSubscribe(int subscribe) {
+        this.subscribe = subscribe;
+    }
+
+    /**
+     * @return 用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间。
+     */
+    public Long getSubscribeTime() {
+        return subscribeTime;
+    }
+
+    public void setSubscribeTime(Long subscribeTime) {
+        this.subscribeTime = subscribeTime;
+    }
+}
