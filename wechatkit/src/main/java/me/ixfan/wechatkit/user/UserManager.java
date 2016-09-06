41ac082561f346bc0209aca87ed1727dfb00e8be
Xiao Fan <fweiwei.026@gmail.com>
2016-09-06 23:19:07 +0800

    获取关注者列表，完成。

--- wechatkit/src/main/java/me/ixfan/wechatkit/user/UserManager.java
+++ wechatkit/src/main/java/me/ixfan/wechatkit/user/UserManager.java
@@ -24,19 +24,69 @@
 
 package me.ixfan.wechatkit.user;
 
+import com.google.gson.JsonObject;
+import me.ixfan.wechatkit.WeChatKitComponent;
+import me.ixfan.wechatkit.common.WeChatConstants;
+import me.ixfan.wechatkit.exceptions.WeChatApiErrorException;
 import me.ixfan.wechatkit.token.TokenManager;
-import me.ixfan.wechatkit.token.WechatAccessTokenContainer;
+import me.ixfan.wechatkit.user.model.WechatFollower;
+import me.ixfan.wechatkit.util.HttpClientUtil;
+import org.apache.commons.codec.binary.StringUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TODO: 用户管理
+ *
  * Created by xfan on 16/3/26.
  */
-public class UserManager {
-
-    private TokenManager tokenManager;
+public class UserManager extends WeChatKitComponent {
 
     public UserManager(TokenManager tokenManager) {
-        this.tokenManager = tokenManager;
+        super(tokenManager);
     }
 
+    /**
+     * 获取关注者列表。关注者列表由一串 OpenID（加密后的微信号，每个用户对每个公众号的OpenID是唯一的）组成。一次拉取调用最多拉取10000个关注者的 OpenID，可以通过多次拉取的方式来满足需求。
+     *
+     * @param nextOpenId 第一个拉取的OPENID，不填默认从头开始拉取。
+     * @return 关注者的 OpenId 数组，最后一个为 <code>next_openid</code>。
+     * @throws WeChatApiErrorException 如果微信API调用失败，返回了错误码和错误信息，会抛出此异常。
+     */
+    public String[] getFollowerList(String nextOpenId) throws WeChatApiErrorException {
+        JsonObject jsonResp;
+        try {
+            final String url = WeChatConstants.WECHAT_GET_USER_LIST
+                                .replace("${ACCESS_TOKEN}", super.tokenManager.getAccessToken());
+            jsonResp = HttpClientUtil.sendGetRequestAndGetJsonResponse(url.replace("${NEXT_OPENID}", null != nextOpenId ? nextOpenId:""));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (jsonResp.has("count") && jsonResp.get("count").getAsInt() > 0) {
+            ArrayList<String> openids = new ArrayList<>();
+            jsonResp.get("data").getAsJsonObject()
+                    .get("openid").getAsJsonArray()
+                    .forEach(e -> openids.add(e.getAsString()));
+            if (jsonResp.has("next_openid")) {
+                openids.add(jsonResp.get("next_openid").getAsString());
+            }
+            return openids.toArray(new String[0]);
+        } else {
+            throw new WeChatApiErrorException(jsonResp.get("errcode").getAsInt(), jsonResp.get("errmsg").getAsString());
+        }
+    }
+
+    /**
+     * TODO 获取微信用户基本信息（包括 UnionId 机制）。同一用户，对同一个微信开放平台下的不同应用，UnionId 是相同的。
+     *
+     * @param openId 用户的 OpenId。
+     * @param lang 返回国家地区语言版本，zh_CN 简体，zh_TW 繁体，en 英语。
+     * @return {@link WechatFollower}
+     * @throws WeChatApiErrorException 如果微信API调用失败，返回了错误码和错误信息，会抛出此异常。
+     */
+    public WechatFollower getUserInfo(String openId, String lang) throws WeChatApiErrorException {
+        return null;
+    }
 }
