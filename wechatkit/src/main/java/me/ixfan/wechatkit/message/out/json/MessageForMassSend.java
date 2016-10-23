32d5c09542e3f07f9c5aab8cdb28d6457a2087b0
Xiao Fan <fweiwei.026@gmail.com>
2016-10-23 14:57:27 +0800

    群发消息对象&JSON序列化

--- wechatkit/src/main/java/me/ixfan/wechatkit/message/out/json/MessageForMassSend.java
+++ wechatkit/src/main/java/me/ixfan/wechatkit/message/out/json/MessageForMassSend.java
@@ -23,8 +23,117 @@
  */
 package me.ixfan.wechatkit.message.out.json;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import me.ixfan.wechatkit.message.out.OutMessageType;
+
+import java.util.List;
+
 /**
  * @author Warren Fan
  */
 public class MessageForMassSend {
+
+    private Filter filter;
+    private String msgType;
+    private String msgContent;
+    private List<String> toUser;
+    private String title;
+    private String description;
+
+    /**
+     * 构建根据分组群发消息的消息对象。
+     *
+     * @param msgType 消息类型。详见{@link OutMessageType}。
+     * @param msgContent 消息内容。
+     * @param groupId 接收消息的分组。
+     * @param isToAll 使用 isToAll 为 true 且成功群发，会使得此次群发进入历史消息列表。
+     */
+    public MessageForMassSend(OutMessageType msgType, String msgContent, int groupId, boolean isToAll)  {
+        this.msgType = msgType.stringValue();
+        this.msgContent = msgContent;
+        this.filter = new Filter(groupId, isToAll);
+    }
+
+    /**
+     * 构建根据 OpenId 列表群发消息的消息对象。
+     *
+     * @param msgType 消息类型。详见{@link OutMessageType}。
+     * @param msgContent 消息内容。
+     * @param toUser 消息的接收者，一串OpenID列表，OpenID最少2个，最多10000个。
+     */
+    public MessageForMassSend(OutMessageType msgType, String msgContent, List<String> toUser) {
+        this.msgType = msgType.stringValue();
+        this.msgContent = msgContent;
+        this.toUser = toUser;
+    }
+
+    public String toJsonString() {
+        Gson gson = new GsonBuilder().registerTypeAdapter(MessageForMassSend.class, new MassMessageGsonTypeAdapter()).create();
+        return gson.toJson(this);
+    }
+
+    public Filter getFilter() {
+        return filter;
+    }
+
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public String getMsgContent() {
+        return msgContent;
+    }
+
+    public void setMsgContent(String msgContent) {
+        this.msgContent = msgContent;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getToUser() {
+        return toUser;
+    }
+
+    static class Filter {
+        private boolean isToAll;
+        private int groupId;
+
+        Filter(int groupId, boolean isToAll) {
+            this.groupId = groupId;
+            this.isToAll = isToAll;
+        }
+
+        public boolean isToAll() {
+            return isToAll;
+        }
+
+        public void setToAll(boolean toAll) {
+            isToAll = toAll;
+        }
+
+        public int getGroupId() {
+            return groupId;
+        }
+
+        public void setGroupId(int groupId) {
+            this.groupId = groupId;
+        }
+    }
+
 }
+
--- wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/MassMessageJsonSerializeTest.java
+++ wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/MassMessageJsonSerializeTest.java
@@ -1,7 +1,129 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.ixfan.wechatkit.message.out.json;
+
+import me.ixfan.wechatkit.message.out.OutMessageType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Warren Fan
  */
+@RunWith(JUnit4.class)
 public class MassMessageJsonSerializeTest {
+
+    private final String NEWS_TO_GROUP = "{\"filter\":{\"is_to_all\":false,\"group_id\":2},\"msgtype\":\"mpnews\",\"mpnews\":{\"media_id\":\"123dsdajkasd231jhksad\"}}";
+    private final String TEXT_TO_GROUP = "{\"filter\":{\"is_to_all\":false,\"group_id\":2},\"msgtype\":\"text\",\"text\":{\"content\":\"CONTENT\"}}";
+    private final String IMG_TO_GROUP = "{\"filter\":{\"is_to_all\":false,\"group_id\":2},\"msgtype\":\"image\",\"image\":{\"media_id\":\"123dsdajkasd231jhksad\"}}";
+    private final String VOICE_TO_GROUP = "{\"filter\":{\"is_to_all\":false,\"group_id\":2},\"msgtype\":\"voice\",\"voice\":{\"media_id\":\"123dsdajkasd231jhksad\"}}";
+    private final String VIDEO_TO_GROUP = "{\"filter\":{\"is_to_all\":false,\"group_id\":2},\"msgtype\":\"mpvideo\",\"mpvideo\":{\"media_id\":\"IhdaAQXuvJtGzwwc0abfXnzeezfO0NgPK6AQYShD8RQYMTtfzbLdBIQkQziv2XJc\"}}";
+    private final String CARD_TO_GROUP = "{\"filter\":{\"is_to_all\":false,\"group_id\":2},\"msgtype\":\"wxcard\",\"wxcard\":{\"card_id\":\"123dsdajkasd231jhksad\"}}";
+
+    private final String NEWS_TO_USERS = "{\"touser\":[\"OPENID1\",\"OPENID2\"],\"msgtype\":\"mpnews\",\"mpnews\":{\"media_id\":\"123dsdajkasd231jhksad\"}}";
+    private final String TEXT_TO_USERS = "{\"touser\":[\"OPENID1\",\"OPENID2\"],\"msgtype\":\"text\",\"text\":{\"content\":\"hello from boxer.\"}}";
+    private final String IMG_TO_USERS = "{\"touser\":[\"OPENID1\",\"OPENID2\"],\"msgtype\":\"image\",\"image\":{\"media_id\":\"BTgN0opcW3Y5zV_ZebbsD3NFKRWf6cb7OPswPi9Q83fOJHK2P67dzxn11Cp7THat\"}}";
+    private final String VOICE_TO_USERS = "{\"touser\":[\"OPENID1\",\"OPENID2\"],\"msgtype\":\"voice\",\"voice\":{\"media_id\":\"mLxl6paC7z2Tl-NJT64yzJve8T9c8u9K2x-Ai6Ujd4lIH9IBuF6-2r66mamn_gIT\"}}";
+    private final String VIDEO_TO_USERS = "{\"touser\":[\"OPENID1\",\"OPENID2\"],\"msgtype\":\"video\",\"video\":{\"media_id\":\"123dsdajkasd231jhksad\",\"title\":\"TITLE\",\"description\":\"DESCRIPTION\"}}";
+    private final String CARD_TO_USERS = "{\"touser\":[\"OPENID1\",\"OPENID2\"],\"msgtype\":\"wxcard\",\"wxcard\":{\"card_id\":\"123dsdajkasd231jhksad\"}}";
+
+    @Test
+    public void successfullySerializeMassTextMsgToGroup() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.TEXT, "CONTENT", 2, false);
+        assertEquals(TEXT_TO_GROUP, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassNewsMsgToGroup() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.MP_NEWS, "123dsdajkasd231jhksad", 2, false);
+        assertEquals(NEWS_TO_GROUP, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassImgMsgToGroup() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.IMAGE, "123dsdajkasd231jhksad", 2, false);
+        assertEquals(IMG_TO_GROUP, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassVoiceMsgToGroup() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.VOICE, "123dsdajkasd231jhksad", 2, false);
+        assertEquals(VOICE_TO_GROUP, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassVideoMsgToGroup() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.MP_VIDEO, "IhdaAQXuvJtGzwwc0abfXnzeezfO0NgPK6AQYShD8RQYMTtfzbLdBIQkQziv2XJc", 2, false);
+        assertEquals(VIDEO_TO_GROUP, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassCardMsgToGroup() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.WX_CARD, "123dsdajkasd231jhksad", 2, false);
+        assertEquals(CARD_TO_GROUP, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassTextMsgToUsers() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.TEXT, "hello from boxer.", Arrays.asList("OPENID1", "OPENID2"));
+        assertEquals(TEXT_TO_USERS, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassNewsMsgToUsers() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.MP_NEWS, "123dsdajkasd231jhksad", Arrays.asList("OPENID1", "OPENID2"));
+        assertEquals(NEWS_TO_USERS, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassImgMsgToUsers() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.IMAGE, "BTgN0opcW3Y5zV_ZebbsD3NFKRWf6cb7OPswPi9Q83fOJHK2P67dzxn11Cp7THat", Arrays.asList("OPENID1", "OPENID2"));
+        assertEquals(IMG_TO_USERS, msg.toJsonString());
+    }
+    @Test
+    public void successfullySerializeMassVoiceMsgToUsers() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.VOICE, "mLxl6paC7z2Tl-NJT64yzJve8T9c8u9K2x-Ai6Ujd4lIH9IBuF6-2r66mamn_gIT", Arrays.asList("OPENID1", "OPENID2"));
+        assertEquals(VOICE_TO_USERS, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassVideoMsgToUsers() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.VIDEO, "123dsdajkasd231jhksad", Arrays.asList("OPENID1", "OPENID2"));
+        msg.setTitle("TITLE");
+        msg.setDescription("DESCRIPTION");
+        assertEquals(VIDEO_TO_USERS, msg.toJsonString());
+    }
+
+    @Test
+    public void successfullySerializeMassCardMsgToUsers() {
+        MessageForMassSend msg = new MessageForMassSend(OutMessageType.WX_CARD, "123dsdajkasd231jhksad", Arrays.asList("OPENID1", "OPENID2"));
+        assertEquals(CARD_TO_USERS, msg.toJsonString());
+    }
+
 }
