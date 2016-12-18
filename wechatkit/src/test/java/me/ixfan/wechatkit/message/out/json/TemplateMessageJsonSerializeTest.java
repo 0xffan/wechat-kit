0128b86c1c28e745cbe5cd336171a0da8811e0c3
Xiao Fan <fweiwei.026@gmail.com>
2016-12-18 17:55:55 +0800

    Add some test case for template message.

--- wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/TemplateMessageJsonSerializeTest.java
+++ wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/TemplateMessageJsonSerializeTest.java
@@ -24,17 +24,20 @@
 
 package me.ixfan.wechatkit.message.out.json;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import me.ixfan.wechatkit.message.out.template.MessageTemplate;
 import me.ixfan.wechatkit.message.out.template.TemplateMessageForSend;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -45,6 +48,7 @@ public class TemplateMessageJsonSerializeTest {
 
     private final String TPL_MSG_POST_DATA = "{\"touser\":\"OPENID\",\"template_id\":\"ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY\",\"url\":\"http://weixin.qq.com/download\",\"data\":{\"first\":{\"value\":\"恭喜你购买成功！\",\"color\":\"#173177\"},\"keynote1\":{\"value\":\"巧克力\",\"color\":\"#173177\"},\"keynote2\":{\"value\":\"39.8元\",\"color\":\"#173177\"},\"keynote3\":{\"value\":\"2014年9月22日\",\"color\":\"#173177\"},\"remark\":{\"value\":\"欢迎再次购买！\"}}}";
     private final String TPL_MSG_POST_DATA_NO_URL = "{\"touser\":\"OPENID\",\"template_id\":\"ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY\",\"url\":\"\",\"data\":{\"first\":{\"value\":\"恭喜你购买成功！\",\"color\":\"#173177\"},\"keynote1\":{\"value\":\"巧克力\",\"color\":\"#173177\"},\"keynote2\":{\"value\":\"39.8元\",\"color\":\"#173177\"},\"keynote3\":{\"value\":\"2014年9月22日\",\"color\":\"#173177\"},\"remark\":{\"value\":\"欢迎再次购买！\"}}}";
+    private final String TPL_LIST_JSON = "{\"template_list\":[{\"template_id\":\"iPk5sOIt5X_flOVKn5GrTFpncEYTojx6ddbt8WYoV5s\",\"title\":\"领取奖金提醒\",\"primary_industry\":\"IT科技\",\"deputy_industry\":\"互联网|电子商务\",\"content\":\"{{result.DATA}}\\n\\n领奖金额:{{withdrawMoney.DATA}}\\n领奖时间:{{withdrawTime.DATA}}\\n银行信息:{{cardInfo.DATA}}\\n到账时间:  {{arrivedTime.DATA} }\\n{{remark.DATA}}\",\"example\":\"您已提交领奖申请\\n\\n领奖金额：xxxx元\\n领奖时间：2013-10-10 12:22:22\\n银行信息：xx银行(尾号xxxx)\\n到账时间：预计xxxxxxx\\n\\n预计将于xxxx到达您的银行卡\"}]}";
 
     @Test
     public void testSerializeTemplateMessageWithoutUrlForSendSuccess() {
@@ -79,5 +83,22 @@ public class TemplateMessageJsonSerializeTest {
         JsonParser jsonParser = new JsonParser();
         JsonObject jsonObject = jsonParser.parse(templateMessageForSend.toJsonString()).getAsJsonObject();
         assertTrue(jsonObject.equals(jsonParser.parse(TPL_MSG_POST_DATA).getAsJsonObject()));
+    }
+
+    @Test
+    public void testDeserializeMessageTemplateListInJsonSuccess() {
+        JsonObject jsonObject = new JsonParser().parse(TPL_LIST_JSON).getAsJsonObject();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+        List<MessageTemplate> tplList = gson.fromJson(jsonObject.getAsJsonArray("template_list"), new TypeToken<ArrayList<MessageTemplate>>(){}.getType());
+        assertTrue("Failed to parse message template list in JSON!", tplList.size() == 1);
+        if (tplList.size() > 0) {
+            MessageTemplate mt = tplList.get(0);
+            assertEquals("iPk5sOIt5X_flOVKn5GrTFpncEYTojx6ddbt8WYoV5s", mt.getTemplateId());
+            assertEquals("领取奖金提醒", mt.getTitle());
+            assertEquals("IT科技", mt.getPrimaryIndustry());
+            assertEquals("互联网|电子商务", mt.getDeputyIndustry());
+            assertEquals("{{result.DATA}}\n\n领奖金额:{{withdrawMoney.DATA}}\n领奖时间:{{withdrawTime.DATA}}\n银行信息:{{cardInfo.DATA}}\n到账时间:  {{arrivedTime.DATA} }\n{{remark.DATA}}", mt.getContent());
+            assertEquals("您已提交领奖申请\n\n领奖金额：xxxx元\n领奖时间：2013-10-10 12:22:22\n银行信息：xx银行(尾号xxxx)\n到账时间：预计xxxxxxx\n\n预计将于xxxx到达您的银行卡", mt.getExample());
+        }
     }
 }
--- wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/TemplateMessageTest.java
+++ wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/TemplateMessageTest.java
@@ -1,7 +1,105 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.ixfan.wechatkit.message.out.json;
+
+import me.ixfan.wechatkit.WeChatKit;
+import me.ixfan.wechatkit.common.WeChatApiResult;
+import me.ixfan.wechatkit.exceptions.WeChatApiErrorException;
+import me.ixfan.wechatkit.message.out.template.MessageTemplate;
+import me.ixfan.wechatkit.token.SimpleTokenContainer;
+import me.ixfan.wechatkit.util.AppProperties;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 /**
  * @author Warren Fan
  */
+@RunWith(JUnit4.class)
 public class TemplateMessageTest {
+
+    private WeChatKit weChatKit = WeChatKit.build(AppProperties.get("WECHAT_ID"),
+                                                  AppProperties.get("APPID"),
+                                                  AppProperties.get("APPSECRET"),
+                                                  SimpleTokenContainer.getTokenContainer());
+    private final String myOpenId = "op8KJwO3XUAMMsvDXqwVMWskLxV0";
+
+    @Ignore("测试通过")
+    @Test
+    public void testGetMessageTemplatesSuccess() {
+        List<MessageTemplate> msgTpls;
+        try {
+            msgTpls = weChatKit.messageManager().retrieveMessageTemplates();
+        } catch (WeChatApiErrorException e) {
+            fail(e.getMessage());
+            return;
+        }
+        assertNotNull(msgTpls);
+    }
+
+    @Ignore("测试通过")
+    @Test
+    public void testSendTemplateMessageSuccess() {
+        Map<String, String> contentParams = new HashMap<>();
+        contentParams.put("title#06D6A0", "Hello, World!");
+        contentParams.put("nickname#EF476F", "X.F.");
+        contentParams.put("gender#26547C", "男");
+        contentParams.put("city", "成都");
+        contentParams.put("remark", "查看详情了解更多信息");
+        WeChatApiResult result = weChatKit.messageManager().sendTemplateMessage("7hdW0wvApI6VVdJyyooLyvu7GD1z66Vc1ZoDzSrUdSg", myOpenId, contentParams);
+        assertNotNull(result);
+        if (null != result) {
+            assertNotNull(result.getMsgId());
+        }
+    }
+
+    @Ignore("测试通过")
+    @Test
+    public void testSendTemplateMessageWithUrlSuccess() {
+        Map<String, String> contentParams = new HashMap<>();
+        contentParams.put("title#06D6A0", "Hello, World!");
+        contentParams.put("nickname#EF476F", "X.F.");
+        contentParams.put("gender#26547C", "男");
+        contentParams.put("city", "成都");
+        contentParams.put("remark", "查看详情了解更多信息");
+        WeChatApiResult result = weChatKit.messageManager().sendTemplateMessage("7hdW0wvApI6VVdJyyooLyvu7GD1z66Vc1ZoDzSrUdSg",
+                                                                                myOpenId,
+                                                                                "https://mp.weixin.qq.com/wiki",
+                                                                                contentParams);
+        assertNotNull(result);
+        if (null != result) {
+            assertNotNull(result.getMsgId());
+        }
+    }
+
 }
