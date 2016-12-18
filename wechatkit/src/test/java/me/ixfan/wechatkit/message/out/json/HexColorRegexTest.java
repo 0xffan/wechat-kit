82ac2e69eb17d3221bc790173b45ae2625bcc2d3
Xiao Fan <fweiwei.026@gmail.com>
2016-12-18 14:24:05 +0800

    template message related testings.

--- wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/HexColorRegexTest.java
+++ wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/HexColorRegexTest.java
@@ -1,7 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.ixfan.wechatkit.message.out.json;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Warren Fan
  */
+@RunWith(JUnit4.class)
 public class HexColorRegexTest {
+
+    private final Pattern hexColorPattern = Pattern.compile("^(.+)(#([a-fA-F0-9]{6}|[a-fA-F0-9]{3}))$");
+    private final List<String> paramsWithSpecifiedColor =
+                    Arrays.asList("paramName#f1f1f1", "paramName#fff", "paramName#333333",
+                                  "paramName#3cb371", "paramName#333", "paramName#556B2F",
+                                  "paramName#708090", "paramName#AF3", "paramName#3Cbe7A",
+                                  "first#173177", "keynote1#173177", "remark#173177");
+
+    @Test
+    public void testParamWithHexColorLengthOf6IsMatch() {
+        Matcher matcher = hexColorPattern.matcher("paramName#f3f1f3");
+        assertTrue(matcher.matches());
+        assertEquals("Matched wrong parameter name!", "paramName", matcher.group(1));
+        assertEquals("Matched wrong color!", "#f3f1f3", matcher.group(2));
+    }
+
+    @Test
+    public void testParamWithHexColorLengthOf3IsMatch() {
+        Matcher matcher = hexColorPattern.matcher("paramName#fff");
+        assertTrue(matcher.matches());
+        assertEquals("Matched wrong parameter name!", "paramName", matcher.group(1));
+        assertEquals("Matched wrong color!", "#fff", matcher.group(2));
+    }
+
+    @Test
+    public void testBunchParamsWithSpecifiedColorAreMatch() {
+        for (String s: paramsWithSpecifiedColor) {
+            Matcher matcher = hexColorPattern.matcher(s);
+            assertTrue(matcher.matches());
+        }
+    }
+
+    @Test
+    public void testParamWithoutColorIsNotMatch() {
+        Matcher matcher = hexColorPattern.matcher("paramName");
+        assertFalse(matcher.matches());
+    }
 }
--- wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/TemplateMessageJsonSerializeTest.java
+++ wechatkit/src/test/java/me/ixfan/wechatkit/message/out/json/TemplateMessageJsonSerializeTest.java
@@ -1,7 +1,83 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Warren Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.ixfan.wechatkit.message.out.json;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import me.ixfan.wechatkit.message.out.template.TemplateMessageForSend;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Warren Fan
  */
+@RunWith(JUnit4.class)
 public class TemplateMessageJsonSerializeTest {
+
+    private final String TPL_MSG_POST_DATA = "{\"touser\":\"OPENID\",\"template_id\":\"ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY\",\"url\":\"http://weixin.qq.com/download\",\"data\":{\"first\":{\"value\":\"恭喜你购买成功！\",\"color\":\"#173177\"},\"keynote1\":{\"value\":\"巧克力\",\"color\":\"#173177\"},\"keynote2\":{\"value\":\"39.8元\",\"color\":\"#173177\"},\"keynote3\":{\"value\":\"2014年9月22日\",\"color\":\"#173177\"},\"remark\":{\"value\":\"欢迎再次购买！\"}}}";
+    private final String TPL_MSG_POST_DATA_NO_URL = "{\"touser\":\"OPENID\",\"template_id\":\"ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY\",\"url\":\"\",\"data\":{\"first\":{\"value\":\"恭喜你购买成功！\",\"color\":\"#173177\"},\"keynote1\":{\"value\":\"巧克力\",\"color\":\"#173177\"},\"keynote2\":{\"value\":\"39.8元\",\"color\":\"#173177\"},\"keynote3\":{\"value\":\"2014年9月22日\",\"color\":\"#173177\"},\"remark\":{\"value\":\"欢迎再次购买！\"}}}";
+
+    @Test
+    public void testSerializeTemplateMessageWithoutUrlForSendSuccess() {
+        Map<String, String> params = new HashMap<>();
+        params.put("first#173177", "恭喜你购买成功！");
+        params.put("keynote1#173177", "巧克力");
+        params.put("keynote2#173177", "39.8元");
+        params.put("keynote3#173177", "2014年9月22日");
+        params.put("remark", "欢迎再次购买！");
+        TemplateMessageForSend templateMessageForSend =
+                new TemplateMessageForSend("ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY",
+                                           "OPENID",
+                                           params);
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = jsonParser.parse(templateMessageForSend.toJsonString()).getAsJsonObject();
+        assertTrue(jsonObject.equals(jsonParser.parse(TPL_MSG_POST_DATA_NO_URL).getAsJsonObject()));
+    }
+
+    @Test
+    public void testSerializeTemplateMessageWithUrlForSendSuccess() {
+        Map<String, String> params = new HashMap<>();
+        params.put("first#173177", "恭喜你购买成功！");
+        params.put("keynote1#173177", "巧克力");
+        params.put("keynote2#173177", "39.8元");
+        params.put("keynote3#173177", "2014年9月22日");
+        params.put("remark", "欢迎再次购买！");
+        TemplateMessageForSend templateMessageForSend =
+                new TemplateMessageForSend("ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY",
+                                           "OPENID",
+                                           "http://weixin.qq.com/download",
+                                           params);
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = jsonParser.parse(templateMessageForSend.toJsonString()).getAsJsonObject();
+        assertTrue(jsonObject.equals(jsonParser.parse(TPL_MSG_POST_DATA).getAsJsonObject()));
+    }
 }
